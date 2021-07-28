be7a4d4bcbd73ce424f3d25670c7363b0bd2cc0f
burya <boztasburhan@gmail.com>
2021-07-28 12:30:26 +0200

    ders notlari updated 28.07.2021

---
+++ python/class-notes/For loop/for loop 26.07.2021.py
@@ -0,0 +1,64 @@
+evens = []
+odds = []
+
+for n in range(10):
+    if n % 2 == 0:
+        evens.append(n)
+    else:
+        odds.append(n)
+print(evens)
+print(odds)
+
+                 #yukaridan asagi piramit
+#for i in range(1,10):
+#    print(str(i) * i)
+
+
+
+
+
+
+
+                  #carpim tablosu
+#number = int(input("enter number "))
+
+#for i in range(11):
+#    print("{}x{} = ".format(number, i),number * i)
+
+
+
+
+
+
+
+#text = "clarusway"
+#count = 0
+#for i in text:
+#    count += 1
+#    if count < len(text):
+#        i = i + "-"
+#    print(i, end = "")
+    
+
+
+
+
+
+
+#numbers = []
+##
+#for i in range(1,6):
+#    numbers.append(i)
+#print(* numbers) # * list den cikarip biseyler yapiyor
+
+
+
+
+
+
+
+
+
+#names = ["Ahmed", "Aisha", "Adam", "Joseph", "Gabriel"]
+#for i in names:
+#    print("Hello", i)
---
+++ python/class-notes/While loop/while loop 26.07.2021.py
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+#                    en uzun kelime
+
+#sentence = input("give me a sentence without any punctuation: ")
+#words = sentence.split()
+#i = 0
+#longest = 0
+
+#while i < len(words):
+#    if len(words[i]) > longest:
+#        longest = len(words[i])
+#    i += 1
+#print("the longest word is: ",longest)
+
+
+
+
+
+                          #taskkk
+#number = 33
+#while True:
+#    guess = int(input("Write what number i am guessing: "))
+#
+#    if guess > number:
+#        print("little high")
+#    elif guess < number:
+#        print ("little lower")
+#    else:
+#        print("you are right")
+#        break
+
+
+
+
+
+
+
+#                   task:yas dogru girme
+
+#age = input("write your age: ")
+#while not age.isdigit():
+#    print("You entered incorrect")
+#    age = input("write your age: ")
+#    print("you are {} years old".format(age))
+
+
+
+
+#number = 0
+#while number < 6:
+#    print(number)
+#    number += 1
+#print("now, number is bigger than or equal to 6")
